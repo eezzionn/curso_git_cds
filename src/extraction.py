5ebdfc0cf4ee50940295c4ce26f75044b0ff36d0
eezzionn <eezzionn@gmail.com>
2023-04-13 09:25:10 -0300

    alterado linhas de comando

--- src/extraction.py
+++ src/extraction.py
@@ -3,7 +3,5 @@ import pandas as pd
 
 
 def load_data():
-    return pd.read_csv("data/bikes_completed.csv")
+    return pd.read_csv("data/processed/bikes_completed.csv")
 
-if __name__ == "__main__":
-    create_main_layout()
